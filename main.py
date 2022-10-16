c680d533f61391db57f8d5ad186e9102e164d4ac
Sam6900 <85671637+Sam6900@users.noreply.github.com>
2022-10-16 15:20:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36",
     "Accept-Language": "en-US,en;q=0.7"
 }
+
 response = requests.get(prod_url, headers=headers)
 soup = BeautifulSoup(response.text, "html.parser")
 prod_title_tag = soup.find(name="span", id="productTitle")
@@ -24,4 +25,4 @@ if price_amt < target_price:
     with smtplib.SMTP("smtp.gmail.com") as connection:
         connection.starttls()
         connection.login(user=my_email, password=my_password)
-        connection.sendmail(from_addr=my_email, to_addrs="sahilsharma220202004@gmail.com", msg=message)
+        connection.sendmail(from_addr=my_email, to_addrs="yourMail@gmail.com", msg=message)
